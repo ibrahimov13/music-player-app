0b65b3982134514d52325467d9ddaf29f59784e3
Sayyad Ibrahimov <69716805+ibrahimov13@users.noreply.github.com>
2023-02-19 22:50:18 +0400

    Add Music Player App all files

---
+++ music.js
@@ -0,0 +1,14 @@
+class Music{
+    constructor(title, singer, img, file){
+        this.title = title;
+        this.singer = singer;
+        this.img = img;
+        this.file = file;
+    }
+
+    getName(){
+        return this.title + " - " + this.singer;
+    }
+}
+
+const musicList = []
